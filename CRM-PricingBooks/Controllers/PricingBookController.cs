7b9efb2082e25650917ff37954d00fcef155a3d9
Melisa Rojas <melisa.rs.1990@gmail.com>
2020-04-18 19:24:53 -0400

    Adding Methods in PricingBookController Class, Create DTOModels file and create PricingBookDTO, ProductoPriceDTO Class into DTOModels

--- CRM-PricingBooks/Controllers/PricingBookController.cs
+++ CRM-PricingBooks/Controllers/PricingBookController.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using CRM_PricingBooks.BusinessLogic;
+using CRM_PricingBooks.Controllers.DTOModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CRM_PricingBooks.Controllers
 {
-    public class PricingBookController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricingBookController : ControllerBase
     {
+        private readonly IPriceLogic _priceLogic;
+
+        public PricingBookController(IPriceLogic pricelogic)
+        {
+            _priceLogic = pricelogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<PricingBookDTO> GetAll()
+        {
+            return _priceLogic.GetPriceProducts();
+        }
     }
 }
