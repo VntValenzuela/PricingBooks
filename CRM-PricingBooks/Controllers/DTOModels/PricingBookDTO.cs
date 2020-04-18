a322686418bf6e41c5272515099b2a0de8f9f510
Melisa Rojas <melisa.rs.1990@gmail.com>
2020-04-18 19:35:32 -0400

    Adding Atributes in PricingBookDTO and ProductPriceDTO Classes

--- CRM-PricingBooks/Controllers/DTOModels/PricingBookDTO.cs
+++ CRM-PricingBooks/Controllers/DTOModels/PricingBookDTO.cs
@@ -7,5 +7,10 @@ namespace CRM_PricingBooks.Controllers.DTOModels
 {
     public class PricingBookDTO
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<ProductPriceDTO> ProductPrices { get; set; }
     }
 }
--- CRM-PricingBooks/Controllers/DTOModels/ProductPriceDTO.cs
+++ CRM-PricingBooks/Controllers/DTOModels/ProductPriceDTO.cs
@@ -7,5 +7,11 @@ namespace CRM_PricingBooks.Controllers.DTOModels
 {
     public class ProductPriceDTO
     {
+        public string ProductCode { get; set; }
+
+        public double FixedPrice { get; set; }
+
+        public double PromotionPrice { get; set; }
+
     }
 }
